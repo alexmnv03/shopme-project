0e593196a43f323005f47d68a13df0d3d4d14501
alex_man <alexmnsbs@gmail.com>
2023-06-02 17:03:16 +0300

    feat: add Constants

--- Common/src/main/java/com/shopme/common/entity/User.java
+++ Common/src/main/java/com/shopme/common/entity/User.java
@@ -8,9 +8,6 @@ import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
@@ -18,7 +15,6 @@ import javax.persistence.Transient;
 
 import com.shopme.common.constants.Constants;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
